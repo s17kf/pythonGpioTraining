a9b0b654f754182974d88aab871e17b863b90465
s17kf <s17kf@users.noreply.github.com>
2018-11-04 16:32:15 +0100

    LCD class upgrade autoscrolling of text longer than display width added new interface of lcd clock.py upgraded to work with new interface of lcd

--- lcd/clock.py
+++ lcd/clock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #import Adafruit_CharLCD as AdafruitLCD
 from modules import LCD
-
+import signal, sys
 #from time import sleep
 import time
 
@@ -16,17 +16,32 @@ lcd_backlight = 4
 lcd_columns = 16
 lcd_rows = 2
 
-
-
 lcd = LCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
+def sigintHandler(signal, frame):
+    print 'ending clock.py by SIGINT'
+    lcd.stop()
+    print 'waiting for lcd will end'
+    lcd.join()
+    print 'end of program'
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigintHandler)
+
+lcd.start()
+localtime = time.asctime(time.localtime(time.time()))
+firstLine = localtime[:4] + localtime[8:11] + localtime[4:8] + localtime[20:]
+secondLine = localtime[11:19]
+lcd.set([firstLine, secondLine])
+
 while True:
-    localtime = time.asctime(time.localtime(time.time()))
-    firstLine = localtime[:4] + localtime[8:11] + localtime[4:8] + localtime[20:]
-    secondLine = localtime[11:19]
+    localtime = time.asctime(time.localtime(time.time()))[11:19]
+#    firstLine = localtime[:4] + localtime[8:11] + localtime[4:8] + localtime[20:]
+#    secondLine = localtime[11:19]
+    lcd.setAtPoint(0,1,localtime)    
     #lcd.clear()
-    lcd.set_cursor(0,0)
-    lcd.message(firstLine + '\n' + secondLine)
+#    lcd.set_cursor(0,0)
+#    lcd.message(firstLine + '\n' + secondLine)
     time.sleep(0.33)
 
 
--- lcd/modules/LCD.py
+++ lcd/modules/LCD.py
@@ -1,13 +1,10 @@
 import Adafruit_CharLCD as AdafruitLCD
 import threading
 import Queue
-
-#from time import sleep
 import time
 
 class LCD(threading.Thread):
     def __init__(self, rs, en, d4, d5, d6, d7, columns, rows, backlight = None, invert_polarity = True, enable_pwm = False, gpio = AdafruitLCD.GPIO.get_platform_gpio(), pwm = AdafruitLCD.PWM.get_platform_pwm(), initial_backlight = 1.0):
-        #AdafruitLCD.Adafruit_CharLCD.__init__(self, rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
         threading.Thread.__init__(self)
         self.lcd = AdafruitLCD.Adafruit_CharLCD(rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
         self.rows = rows
@@ -17,39 +14,37 @@ class LCD(threading.Thread):
         self.isWorking.set()
         localtime = time.asctime(time.localtime(time.time()))
         self.lines = []
-        self.scroll = []
-        self.sizes = []
-        self.isScrollNeed = threading.Event()
+        self.scroll = 0
+        self.lastScrollTime = time.time()
+        self.max_size = 0
+        self.__autoscroll = True
         for i in range(self.rows):
             self.lines.append("")
-            self.scroll.append(0)
-            self.sizes.append(0)
 
     def run(self):
         while self.isWorking.is_set():
             try:
-                new_item = self.queue.get(True, 1)
+                new_item = self.queue.get(True, 0.9)
             except Queue.Empty:
-#                if isScrollNeed.is_set():
-#                    for i in range(self.rows):
-#                        if self.sizes[i] > self.cols:
-#                            if self.sizes[i] - self.cols > self.scroll[i]:
-                pass                              
+                self.__scroll()
             except:
                 print 'other error'
             else:
                 if new_item[0] == self.rows:
                     if new_item[3] == True:
                         self.lcd.clear()
-                    self.isScrollNeed.clear()
+                    else:
+                        for i in range(self.scroll):
+                            self.lcd.move_left
+                    self.max_size = 0
                     for i in range(self.rows):
                         self.lines[i] = new_item[i+1]
                         self.lcd.set_cursor(0, i)
                         self.lcd.message(self.lines[i])
-                        self.sizes[i] = len(self.lines[i])
-                        if self.sizes[i] > self.cols:
-                            self.isScrollNeed.set()
-                        self.scroll[i] = 0
+                        if len(self.lines[i]) > self.max_size:
+                            self.max_size = len(self.lines[i])
+                    self.scroll = 0
+                    self.__setLastScrollTime(time.time() + 1)
                 elif new_item[0] >= 0 and new_item[0] < self.rows:
                     self.lines[new_item[0]] = new_item[1]
                     if new_item[2] == True:
@@ -58,24 +53,47 @@ class LCD(threading.Thread):
                             self.lcd.set_cursor(0, i)
                             self.lcd.message(self.lines(i))
                     else:
+                        for i in range(self.scroll):
+                            self.lcd.move_left
+                        for line in self.lines:
+                            if len(line) > self.max_size:
+                                self.max_size = len(line)
                         self.lcd.set_cursor(0, new_item[0])
                         self.lcd.message(new_item[1])
+                    self.scroll = 0
+                    self.__setLastScrollTime(time.time() + 1)
                 elif new_item[0] == -1:
                     if new_item[4] == True:
                         self.lcd.clear()
                     self.lcd.set_cursor(new_item[1], new_item[2])
                     self.lcd.message(new_item[3])
+                    self.__scroll()
                 #TODO obsluga linii
-                #TODO scroll too long text
             
-            
+    def __scroll(self):
+        if self.__autoscroll and self.max_size > self.cols and time.time() - self.lastScrollTime > 0.8:
+            if self.scroll < self.max_size - self.cols:
+                self.lcd.move_left()
+                self.scroll += 1
+                self.__setLastScrollTime(time.time())
+            else:
+                for i in range(self.scroll):
+                    self.lcd.move_right()
+                self.scroll = 0
+                self.__setLastScrollTime(time.time() + 1)
 
+    def __setLastScrollTime(self, scrollTime = time.time()):
+        self.lastScrollTime = scrollTime
 
     def stop(self):
         self.isWorking.clear()
 
-    def set(self, text0, text1, clear = True):
-        self.queue.put([self.rows, text0, text1, clear])
+    def set(self, text, clear = True):
+        new_item = [self.rows]
+        for i in range(self.rows):
+            new_item.append(text[i])
+        new_item.append(clear)
+        self.queue.put(new_item)
 
     def setLine(self, line, text, clear = True):
         if line < 0 or line >= self.rows:
@@ -85,6 +103,14 @@ class LCD(threading.Thread):
     
     def setAtPoint(self, col, row, text, clear = False):
         self.queue.put([-1, col, row, text, clear])
+
+    def turnOn(self, backlight = 1):
+        self.lcd.set_backlight(backlight)
+    def turnOff(self):
+        self.lcd.set_backlight(0)
+
+    def setAutoscroll(self, autoscroll):
+        self.__autoscroll = autoscroll
 
     def moveLeft(self):
         self.lcd.move_left()
