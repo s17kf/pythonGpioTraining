af2eaf1f8e61aa9b1548a1dc91a99ff1f8a9957a
s17kf <s17kf@users.noreply.github.com>
2018-11-04 11:54:08 +0100

    Some cleanup and LCD class upgrade

--- lcd/modules/LCD.py
+++ lcd/modules/LCD.py
@@ -1,48 +1,96 @@
 import Adafruit_CharLCD as AdafruitLCD
 import threading
+import Queue
 
 #from time import sleep
-#import time
+import time
 
-lcd_rs = 25
-lcd_en = 24
-lcd_d4 = 23
-lcd_d5 = 17
-lcd_d6 = 21
-lcd_d7 = 22
-lcd_backlight = 4
-
-lcd_columns = 16
-lcd_rows = 2
-
-class LCD(AdafruitLCD.Adafruit_CharLCD, threading.Thread):
+class LCD(threading.Thread):
     def __init__(self, rs, en, d4, d5, d6, d7, columns, rows, backlight = None, invert_polarity = True, enable_pwm = False, gpio = AdafruitLCD.GPIO.get_platform_gpio(), pwm = AdafruitLCD.PWM.get_platform_pwm(), initial_backlight = 1.0):
-#        super(LCD, self).__init__(rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
-        AdafruitLCD.Adafruit_CharLCD.__init__(self, rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
-#        super(AdafruitLCD.Adafruit_CharLCD, self).__init__(rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
-           
+        #AdafruitLCD.Adafruit_CharLCD.__init__(self, rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
+        threading.Thread.__init__(self)
+        self.lcd = AdafruitLCD.Adafruit_CharLCD(rs, en, d4, d5, d6, d7, columns, rows, backlight, invert_polarity, enable_pwm, gpio, pwm, initial_backlight)
+        self.rows = rows
+        self.cols = columns
+        self.queue = Queue.Queue()
+        self.isWorking = threading.Event()
+        self.isWorking.set()
+        localtime = time.asctime(time.localtime(time.time()))
+        self.lines = []
+        self.scroll = []
+        self.sizes = []
+        self.isScrollNeed = threading.Event()
+        for i in range(self.rows):
+            self.lines.append("")
+            self.scroll.append(0)
+            self.sizes.append(0)
+
+    def run(self):
+        while self.isWorking.is_set():
+            try:
+                new_item = self.queue.get(True, 1)
+            except Queue.Empty:
+#                if isScrollNeed.is_set():
+#                    for i in range(self.rows):
+#                        if self.sizes[i] > self.cols:
+#                            if self.sizes[i] - self.cols > self.scroll[i]:
+                pass                              
+            except:
+                print 'other error'
+            else:
+                if new_item[0] == self.rows:
+                    if new_item[3] == True:
+                        self.lcd.clear()
+                    self.isScrollNeed.clear()
+                    for i in range(self.rows):
+                        self.lines[i] = new_item[i+1]
+                        self.lcd.set_cursor(0, i)
+                        self.lcd.message(self.lines[i])
+                        self.sizes[i] = len(self.lines[i])
+                        if self.sizes[i] > self.cols:
+                            self.isScrollNeed.set()
+                        self.scroll[i] = 0
+                elif new_item[0] >= 0 and new_item[0] < self.rows:
+                    self.lines[new_item[0]] = new_item[1]
+                    if new_item[2] == True:
+                        self.lcd.clear()
+                        for i in range(self.rows):
+                            self.lcd.set_cursor(0, i)
+                            self.lcd.message(self.lines(i))
+                    else:
+                        self.lcd.set_cursor(0, new_item[0])
+                        self.lcd.message(new_item[1])
+                elif new_item[0] == -1:
+                    if new_item[4] == True:
+                        self.lcd.clear()
+                    self.lcd.set_cursor(new_item[1], new_item[2])
+                    self.lcd.message(new_item[3])
+                #TODO obsluga linii
+                #TODO scroll too long text
+            
+            
 
 
-#lcd = LCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-#lcd = LCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight, True, True)
-#lcd = AdafruitLCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-#lcd = AdafruitLCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight, True, True)
+    def stop(self):
+        self.isWorking.clear()
 
-#localtime = time.localtime(time.time())
-#localtime = time.asctime(localtime)
-#print localtime
+    def set(self, text0, text1, clear = True):
+        self.queue.put([self.rows, text0, text1, clear])
 
-#lcd.message(localtime)
-#time.sleep(0.5)
+    def setLine(self, line, text, clear = True):
+        if line < 0 or line >= self.rows:
+            raise IndexError, 'only 0-1 lines available'
+            return
+        self.queue.put([line, text, clear])
+    
+    def setAtPoint(self, col, row, text, clear = False):
+        self.queue.put([-1, col, row, text, clear])
 
-#lcd.set_backlight(0)
+    def moveLeft(self):
+        self.lcd.move_left()
 
-#for i in range(11):
-#    lcd.set_backlight(i/10.0)
-#    time.sleep(0.7)
-
-
-
+    def moveRight(self):
+        self.lcd.move_right()
 
 
 
